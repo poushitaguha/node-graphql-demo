9a54927bf0b1ed1e4c02eae213417991e2a7445e
poushitaguha <pguha.kolkata@gmail.com>
2019-11-06 15:03:15 -0500

    Create graphql schema

--- index.js
+++ index.js
@@ -32,36 +32,38 @@ const contactType = new graphql.GraphQLObjectType({
 let queryType = new graphql.GraphQLObjectType({
     name: "Query",
     fields: {
-        type: graphql.GraphQLList(contactType),
-        resolve: (root, args, context, info) => {
-            return new Promise((resolve, reject) => {
-                database.all("SELECT * FROM contacts;", function (err, rows) {
-                    if (err) {
-                        reject([]);
-                    }
-                    resolve(rows);
+        contacts: {
+            type: graphql.GraphQLList(contactType),
+            resolve: (root, args, context, info) => {
+                return new Promise((resolve, reject) => {
+                    database.all("SELECT * FROM contacts;", function (err, rows) {
+                        if (err) {
+                            reject([]);
+                        }
+                        resolve(rows);
+                    });
                 });
-            });
-        }
-    },
-    contact: {
-        type: contactType,
-        args: {
-            id: {
-                type: new graphql.GraphQLNonNull(graphql.GraphQLID)
             }
         },
-        resolve: (root, { id }, context, info) => {
-            return new Promise((resolve, reject) => {
-                database.all("SELECT * FROM contacts where id = (?);", [id], function (err, rows) {
-                    if (err) {
-                        reject(null);
-                    }
-                    resolve(rows[0]);
+        contact: {
+            type: contactType,
+            args: {
+                id: {
+                    type: new graphql.GraphQLNonNull(graphql.GraphQLID)
+                }
+            },
+            resolve: (root, { id }, context, info) => {
+                return new Promise((resolve, reject) => {
+                    database.all("SELECT * FROM contacts where id = (?);", [id], function (err, rows) {
+                        if (err) {
+                            reject(null);
+                        }
+                        resolve(rows[0]);
+                    });
                 });
-            });
+            }
         }
-    }
+    },
 });
 
 // Create a mutation type that corresponds to the create, update and delete operations
@@ -83,7 +85,7 @@ let mutationType = new graphql.GraphQLObjectType({
             },
             resolve: (root, { firstName, lastName, email }) => {
                 return new Promise((resolve, reject) => {
-                    database.run("INSERT INTO contacts (firstName, lastName, email) VALUES (? ? ?);", [firstName, lastName, email], (err) => {
+                    database.run('INSERT INTO contacts (firstName, lastName, email) VALUES (?,?,?);', [firstName, lastName, email], (err) => {
                         if (err) {
                             reject(null);
                         }
@@ -117,7 +119,7 @@ let mutationType = new graphql.GraphQLObjectType({
             },
             resolve: (root, { id, firstName, lastName, email }) => {
                 return new Promise((resolve, reject) => {
-                    database.run("UPDATE contacts SET firstName = (?), lastName = (?), email = (?) WHERE id = (?);", [firstName, lastName, email, id], (err) => {
+                    database.run('UPDATE contacts SET firstName = (?), lastName = (?), email = (?) WHERE id = (?);', [firstName, lastName, email, id], (err) => {
                         if (err) {
                             reject(err);
                         }
@@ -135,7 +137,7 @@ let mutationType = new graphql.GraphQLObjectType({
             },
             resolve: (root, { id }) => {
                 return new Promise((resolve, reject) => {
-                    database.run("DELETE from contacts WHERE id = (?);", [id], (err) => {
+                    database.run('DELETE from contacts WHERE id = (?);', [id], (err) => {
                         if (err) {
                             reject(err);
                         }
@@ -146,3 +148,14 @@ let mutationType = new graphql.GraphQLObjectType({
         }
     }
 });
+
+// Create a GraphQL schema
+const schema = new graphql.GraphQLSchema({
+    query: queryType,
+    mutation: mutationType
+});
+
+app.use("/graphql", expressGraphQL({ schema: schema, graphiql: true }));
+app.listen(4000, () => {
+    console.log("GraphQL server running at http://localhost:4000.");
+})
